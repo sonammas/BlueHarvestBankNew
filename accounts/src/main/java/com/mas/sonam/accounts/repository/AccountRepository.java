5e0195f29b02f6d5b91c0a69dfa229c8fadb76a3
sonam.masuriha <sonam161987@gmail.com>
2018-06-25 00:06:45 +0200

    add transaction service

--- accounts/src/main/java/com/mas/sonam/accounts/repository/AccountRepository.java
+++ accounts/src/main/java/com/mas/sonam/accounts/repository/AccountRepository.java
@@ -4,12 +4,8 @@ import com.mas.sonam.accounts.model.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    List<Account> findByCustomer(Long customerId);
 
     Account findByAccountNumber(int accountNumber);
 
